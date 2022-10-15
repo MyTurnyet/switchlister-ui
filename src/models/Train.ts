ee3926d87afc0475dfd66dbe850ef77c82fe7c7f
Paige Watson <paige@industriallogic.com>
2022-10-15 11:41:09 -0700

    add station names to train

--- src/models/Train.ts
+++ src/models/Train.ts
@@ -1,6 +1,10 @@
 export class Train {
-  constructor(private trainName: string) {}
+  constructor(private trainName: string, private stations: string[] = []) {}
+
   public get name(): string {
     return this.trainName;
+  }
+  public get stationNames(): string[] {
+    return this.stations;
   }
 }
--- src/models/__tests__/Train.test.ts
+++ src/models/__tests__/Train.test.ts
@@ -5,4 +5,17 @@ describe('Train', () => {
     const train = new Train('test name');
     expect(train.name).toEqual('test name');
   });
+
+  it('returns empty list of station names by default', () => {
+    const train = new Train('test name');
+    const stationNames: string[] = train.stationNames;
+    expect(stationNames.length).toEqual(0);
+  });
+
+  it('has a list of defined station names', () => {
+    const expectedNames: string[] = ['station 1', 'station 2'];
+    const train = new Train('test name', expectedNames);
+    const stationNames: string[] = train.stationNames;
+    expect(stationNames).toEqual(expectedNames);
+  });
 });
