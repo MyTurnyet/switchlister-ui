755f03f39476bb457348e46119a7dfd1f6df1789
Paige Watson <paige.watson@outlook.com>
2022-10-22 14:23:34 -0700

    add flex size prop to DisplayRow

--- src/components/TrainCard.tsx
+++ src/components/TrainCard.tsx
@@ -1,5 +1,6 @@
 import { Train } from '../models/Train';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 export interface TrainCardProps {
   train: Train;
@@ -12,39 +13,41 @@ export const TrainCard = ({ train }: TrainCardProps) => {
 
   const NameDiv = styled(trainDisplayDiv)`
     width: 100px;
-    background-color: #61dafb;
   `;
   const StationCountDiv = styled(trainDisplayDiv)`
     width: 100px;
-    background-color: darkseagreen;
-    border: chocolate 2px solid;
   `;
   const StationNameDiv = styled(trainDisplayDiv)`
     width: 100%;
-    background-color: bisque;
     color: green;
   `;
+
+  const dynamicStyle = (props: { flex: number }) => {
+    return css`
+      flex: ${props.flex};
+    `;
+  };
   const DisplayRow = styled.div`
     display: flex;
-    border: red 2px solid;
     justify-content: space-between;
+    ${dynamicStyle};
   `;
 
-  const CardContainer = styled.div`
-    flex-direction: column;
-    background-color: white;
-    display: flex;
-    height: 10vh;
-    width: 20vh;
-  `;
+  const CardContainer = styled.div((props) => ({
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    display: 'flex',
+    height: '10vh',
+    width: '20vh',
+  }));
 
   return (
     <CardContainer>
-      <DisplayRow>
+      <DisplayRow flex={3}>
         <NameDiv>{train.name}</NameDiv>
         <StationCountDiv>{train.stationNames.length} stations</StationCountDiv>
       </DisplayRow>
-      <DisplayRow>
+      <DisplayRow flex={1}>
         <StationNameDiv>{train.stationNames[0]}</StationNameDiv>
       </DisplayRow>
     </CardContainer>
