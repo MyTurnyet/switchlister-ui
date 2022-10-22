2c7462f92ad4cfe7a5d0063c8dc8c7e5dd0d0bdc
Paige Watson <paige.watson@outlook.com>
2022-10-22 16:47:09 -0700

    use emotion styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import './App.css';
 import { TrainGrid } from './components/TrainGrid/TrainGrid';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { TrainPage } from './components/TrainPage/TrainPage';
 
 function App() {
+  const train1 = new Train('Local Express', ['station 1', 'station 2']);
+  const train2 = new Train('Another Train', ['station 14', 'station 2', 'station 21']);
+
+  const MainAppWrapper = styled.div`
+    background-color: #282c34;
+  `;
+  const AppContent = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    color: white;
+  `;
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<TrainGrid />} />
-        <Route path={'/trainpage'} element={<TrainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <MainAppWrapper>
+      <AppContent>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<TrainGrid />} />
+            <Route path={'/trainpage'} element={<TrainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AppContent>
+    </MainAppWrapper>
   );
+import './App.css';
+import { TrainGrid } from './components/TrainGrid/TrainGrid';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { TrainPage } from './components/TrainPage/TrainPage';
+import { Train } from './models/Train';
+import { TrainGrid } from './components/TrainGrid';
+import styled from '@emotion/styled';
 }
 
-export default App;
+export default App
