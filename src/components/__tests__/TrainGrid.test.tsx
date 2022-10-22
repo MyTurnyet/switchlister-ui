70a4e4e39cc96b340ad4cff3572c7c215054ddfa
Paige Watson <paige.watson@outlook.com>
2022-10-22 14:58:51 -0700

    test that all trains are displayed

--- src/components/__tests__/TrainGrid.test.tsx
+++ src/components/__tests__/TrainGrid.test.tsx
@@ -4,15 +4,22 @@ import { TrainGrid } from '../TrainGrid';
 import { Train } from '../../models/Train';
 
 describe('Train Grid', () => {
-  it('displays the first train', () => {
-    const expectedNames: string[] = ['station 1', 'station 2'];
-    const trainName = 'test name';
-    const train = new Train(trainName, expectedNames);
+  const stationNames1: string[] = ['station 1', 'station 2'];
+  const stationNames2: string[] = ['station 3'];
+  const testName1 = 'test name1';
+  const testName2 = 'test name2';
+  const train1 = new Train(testName1, stationNames1);
+  const train2 = new Train(testName2, stationNames2);
 
-    const trainGrid: RenderResult = render(<TrainGrid trains={[train]}></TrainGrid>);
-    trainGrid.getByText(trainName);
-    trainGrid.getByText('2 stations');
-    trainGrid.getByText('station 1');
-    trainGrid.getByText('station 2');
+  const allTrains: Train[] = [train1];
+  it('displays the first train', () => {
+    const trainGrid: RenderResult = render(<TrainGrid trains={allTrains}></TrainGrid>);
+    allTrains.map((train: Train) => {
+      trainGrid.getByText(train.name);
+      trainGrid.getByText(train.stationNames.length + ' stations');
+      train.stationNames.map((stationName) => {
+        trainGrid.getByText(stationName);
+      });
+    });
   });
 });
