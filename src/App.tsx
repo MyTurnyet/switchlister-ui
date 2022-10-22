af0515c999790879091fe7dbcbb0d825fab0f064
Paige Watson <paige.watson@outlook.com>
2022-10-22 13:40:30 -0700

    add emotion styling library

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Train } from './models/Train';
+import { TrainCard } from './components/TrainCard';
 
 function App() {
+  const expectedNames: string[] = ['station 1', 'station 2'];
+  const trainName = 'test name';
+  const train = new Train(trainName, expectedNames);
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <div>
+          <TrainCard train={train} />
+        </div>
       </header>
     </div>
   );
--- src/components/TrainCard.tsx
+++ src/components/TrainCard.tsx
@@ -1,15 +1,53 @@
 import { Train } from '../models/Train';
+import styled from '@emotion/styled';
 
 export interface TrainCardProps {
   train: Train;
 }
 
 export const TrainCard = ({ train }: TrainCardProps) => {
+  const trainDisplayDiv = styled.div`
+    color: brown;
+  `;
+
+  const TrainNameDiv = styled(trainDisplayDiv)`
+    width: 100px;
+    background-color: #61dafb;
+  `;
+  const TrainStationCountDiv = styled(trainDisplayDiv)`
+    width: 100px;
+    background-color: darkseagreen;
+    border: chocolate 2px solid;
+  `;
+  const FirstStationNameDiv = styled(trainDisplayDiv)`
+    width: 300px;
+    background-color: bisque;
+    color: green;
+  `;
+  const TrainCardContainer = styled.div`
+    flex-direction: column;
+
+    background-color: white;
+    display: flex;
+    height: 20vh;
+    width: 20vh;
+  `;
+
+  const TopRow = styled.div`
+    display: flex;
+    border: red 2px solid;
+    justify-content: space-between;
+  `;
+
   return (
-    <div>
-      <div>{train.name}</div>
-      <div>{train.stationNames.length} stations</div>
-      <div>{train.stationNames[0]}</div>
-    </div>
+    <TrainCardContainer>
+      <TopRow>
+        <TrainNameDiv>{train.name}</TrainNameDiv>
+        <TrainStationCountDiv>{train.stationNames.length} stations</TrainStationCountDiv>
+      </TopRow>
+      <TopRow>
+        <FirstStationNameDiv>{train.stationNames[0]}</FirstStationNameDiv>
+      </TopRow>
+    </TrainCardContainer>
   );
 };
