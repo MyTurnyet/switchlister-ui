0efbf30d918ee42b4c8bbc89d0733d64e6b87799
Paige Watson <paige@industriallogic.com>
2022-10-29 10:22:52 -0700

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,11 @@ import { Train } from './models/Train';
 import { TrainGrid } from './components/TrainGrid';
 
 function App() {
-  const train1 = new Train('Local Express', ['station 1', 'station 2']);
-  const train2 = new Train('Another Train', ['station 14', 'station 2', 'station 21']);
-
   return (
     <div className='App'>
       <header className='App-header'>
         <div>
-          <TrainGrid trains={[train1, train2]} />
+          <TrainGrid />
         </div>
       </header>
     </div>
