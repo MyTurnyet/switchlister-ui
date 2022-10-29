35a6c6fc7e7277176b7d465f50829eeb057ca843
Paige Watson <paige@industriallogic.com>
2022-10-29 09:46:21 -0700

    refactor the styled.divs to the bottom of the file (outside the component)

--- src/components/TrainCard.tsx
+++ src/components/TrainCard.tsx
@@ -6,44 +6,6 @@ export interface TrainCardProps {
 }
 
 export const TrainCard = ({ train }: TrainCardProps) => {
-  const trainDisplayDiv = styled.div`
-    color: brown;
-  `;
-
-  const NameDiv = styled(trainDisplayDiv)`
-    width: 100px;
-  `;
-  const StationCountDiv = styled(trainDisplayDiv)`
-    width: 100px;
-  `;
-  const StationNameDiv = styled(trainDisplayDiv)`
-    width: 100%;
-    color: green;
-  `;
-
-  const TopDisplayRow = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex: 3;
-  `;
-  const BottomDisplayRow = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    flex: ${train.stationNames.length};
-    margin-bottom: 10px;
-  `;
-
-  const CardContainer = styled.div`
-    flex-direction: column;
-    background-color: white;
-    display: flex;
-    height: 10vh;
-    width: 20vh;
-    border: black 1px solid;
-    border-radius: 8px;
-  `;
-
   return (
     <CardContainer>
       <TopDisplayRow>
@@ -58,3 +20,41 @@ export const TrainCard = ({ train }: TrainCardProps) => {
     </CardContainer>
   );
 };
+
+const trainDisplayDiv = styled.div`
+  color: brown;
+`;
+
+const NameDiv = styled(trainDisplayDiv)`
+  width: 100px;
+`;
+const StationCountDiv = styled(trainDisplayDiv)`
+  width: 100px;
+`;
+const StationNameDiv = styled(trainDisplayDiv)`
+  width: 100%;
+  color: green;
+`;
+
+const TopDisplayRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex: 3;
+`;
+const BottomDisplayRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  flex: 3;
+  margin-bottom: 10px;
+`;
+
+const CardContainer = styled.div`
+  flex-direction: column;
+  background-color: white;
+  display: flex;
+  height: 10vh;
+  width: 20vh;
+  border: black 1px solid;
+  border-radius: 8px;
+`;
