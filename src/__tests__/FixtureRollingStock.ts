55521652efffd7cad537906af91f92d4ab456f41
Paige Watson <paige@industriallogic.com>
2022-11-19 09:36:10 -0800

    move rolling stock objects into fixture file

---
+++ src/__tests__/FixtureRollingStock.ts
@@ -0,0 +1,21 @@
+import { RollingStock, RollingStockState } from '../models/RollingStock';
+
+export const hopperBCAX5State: RollingStockState = {
+  carType: 'HT',
+  color: 'WHT',
+  roadName: 'BCAX',
+  roadNumber: 5,
+  length: 52,
+  loaded: false,
+};
+export const boxcarCP1234State: RollingStockState = {
+  carType: 'XM',
+  color: 'RED',
+  roadName: 'CPR',
+  roadNumber: 1234,
+  length: 52,
+  loaded: false,
+};
+
+export const boxcarCP1234 = new RollingStock(boxcarCP1234State);
+export const hopperBCAX5 = new RollingStock(hopperBCAX5State);
--- src/models/__tests__/RollingStock.test.ts
+++ src/models/__tests__/RollingStock.test.ts
@@ -1,35 +1,19 @@
-import { RollingStock, RollingStockState } from '../RollingStock';
+import { boxcarCP1234, hopperBCAX5, hopperBCAX5State } from '../../__tests__/FixtureRollingStock';
 
 describe('rolling stock', () => {
-  const hopperBCAX5: RollingStockState = {
-    carType: 'HT',
-    color: 'WHT',
-    roadName: 'BCAX',
-    roadNumber: 5,
-    length: 52,
-    loaded: false,
-  };
-  const boxcarCP1234: RollingStockState = {
-    carType: 'XM',
-    color: 'RED',
-    roadName: 'CPR',
-    roadNumber: 1234,
-    length: 52,
-    loaded: false,
-  };
-  const boxcar = new RollingStock(boxcarCP1234);
-  const hopper = new RollingStock(hopperBCAX5);
   describe('data', () => {
     it('is car type HT', () => {
-      const hasType = hopper.isCarType('HT');
+      const hasType = hopperBCAX5.isCarType('HT');
       expect(hasType).toEqual(true);
     });
     it('is not car type HT', () => {
-      const hasType = boxcar.isCarType('HT');
+      const hasType = boxcarCP1234.isCarType('HT');
       expect(hasType).toEqual(false);
     });
     it('returns display name', () => {
-      expect(hopper.displayName).toEqual(`${hopperBCAX5.roadName} ${hopperBCAX5.roadNumber}`);
+      expect(hopperBCAX5.displayName).toEqual(
+        `${hopperBCAX5State.roadName} ${hopperBCAX5State.roadNumber}`,
+      );
     });
   });
 });
