4539367ef81cb41ed599e4b32d8b3ead41ba2290
Paige Watson <paige@industriallogic.com>
2022-11-22 21:59:28 -0800

    fix render test to return one item from fetch

--- src/data/RollingStockContext.tsx
+++ src/data/RollingStockContext.tsx
@@ -1,6 +1,7 @@
-import React, { createContext, PropsWithChildren, useContext } from 'react';
+import React, { createContext, PropsWithChildren, useContext, useEffect } from 'react';
 import { RollingStockState } from '../models/RollingStock';
 import { useReactState } from '../state-management/ReactState';
+import { log } from 'util';
 
 export interface RollingStockContextState {
   rollingStock: RollingStockState[];
@@ -12,6 +13,22 @@ const RollingStockContext = createContext<RollingStockContextState>(defaultData)
 
 export const RollingStockProvider = ({ children }: PropsWithChildren) => {
   const rollingStockData = useReactState<RollingStockState[]>([]);
+
+  const setData = (data: RollingStockState[]) => {
+    rollingStockData.setValue(data);
+  };
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetch('https://dog.ceo/api/breeds/image/random').then((response) =>
+        response.json(),
+      );
+      console.log(data);
+      setData(data);
+    }
+    getData();
+  }, []);
+
   const returnedState: RollingStockContextState = { rollingStock: rollingStockData.value };
   return (
     <RollingStockContext.Provider value={returnedState}>{children}</RollingStockContext.Provider>
--- src/data/__tests__/RollingStockContext.test.tsx
+++ src/data/__tests__/RollingStockContext.test.tsx
@@ -1,5 +1,17 @@
 import { RollingStockProvider, useRollingStockData } from '../RollingStockContext';
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderResult, waitFor } from '@testing-library/react';
+import { RollingStockState } from '../../models/RollingStock';
+import { hopperBCAX5State } from '../../testData/FixtureRollingStock';
+import { act } from 'react-dom/test-utils';
+
+const allRollingStock: RollingStockState[] = [hopperBCAX5State];
+
+export const assetsFetchMock = () =>
+  Promise.resolve({
+    ok: true,
+    status: 200,
+    json: async () => allRollingStock,
+  } as Response);
 
 const TestRollingStockConsumer = () => {
   const contextState = useRollingStockData();
@@ -15,8 +27,16 @@ function renderWithProvider(): RenderResult {
 }
 
 describe('rolling stock context', () => {
-  it('renders with 0 items', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(assetsFetchMock);
+  });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  it('renders with 0 items', async () => {
     const testConsumer = renderWithProvider();
-    testConsumer.getByText('count: 0');
+    await waitFor(() => {
+      testConsumer.getByText('count: 1');
+    });
   });
 });
