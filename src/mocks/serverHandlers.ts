952d817c1125f49d05cba9dd2217a53124e28df2
Paige Watson <paige@industriallogic.com>
2022-12-01 11:11:28 -0800

    add second car to return values

--- src/mocks/serverHandlers.ts
+++ src/mocks/serverHandlers.ts
@@ -1,8 +1,8 @@
 import { rest } from 'msw';
-import { hopperBCAX5State } from '../testData/FixtureRollingStock';
+import { boxcarCP1234State, hopperBCAX5State } from '../testData/FixtureRollingStock';
 
 export const handlers = [
   rest.get('https://switchlister-api.herokuapp.com/rollingstock', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json([hopperBCAX5State]));
+    return res(ctx.status(200), ctx.json([hopperBCAX5State, boxcarCP1234State]));
   }),
 ];
