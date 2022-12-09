740baa1f095fa82b98e873c3d61e32a8b43b4a99
Paige Watson <paige@industriallogic.com>
2022-12-08 16:12:36 -0800

    create styled divs for AppLayout

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,21 +1,42 @@
 import React, { PropsWithChildren } from 'react';
 import { Outlet } from 'react-router';
+import styled from '@emotion/styled';
 
 export const AppLayout = (props: PropsWithChildren) => {
   return (
     <div>
-      <div>
-        <div>SwitchLister</div>
+      <LayoutContainer>
+        <AppHeader>SwitchLister</AppHeader>
         {/* <NavBar></NavBar>*/}
-      </div>
-      <div title={'content'}>
+      </LayoutContainer>
+      <ContentArea title={'content'}>
         {/* <DataContext>*/}
         {/*  <StoryInformationView />*/}
         {/* </DataContext>*/}
-        <div title={'pageView'}>
+        <PageView title={'pageView'}>
           <Outlet></Outlet>
-        </div>
-      </div>
+        </PageView>
+      </ContentArea>
     </div>
   );
 };
+
+const LayoutContainer = styled.div`
+  text-align: center;
+  height: 12vh;
+  width: 100vw;
+  justify-content: center;
+  border-bottom: #1565c0 3px solid;
+`;
+const AppHeader = styled.div`
+  font-size: calc(10px + 2vmin);
+  margin: 12px 8px 4px 0;
+`;
+const ContentArea = styled.div`
+  display: flex;
+  height: 86vh;
+  flex-direction: row;
+`;
+const PageView = styled.div`
+  flex: 5;
+`;
