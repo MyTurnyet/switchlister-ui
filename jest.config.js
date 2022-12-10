aa6c286f29df30f8bea6c9147fae903618c00144
Paige Watson <paige.watson@outlook.com>
2022-12-10 09:59:41 -0800

    fix importing of display name

--- jest.config.js
+++ jest.config.js
@@ -13,13 +13,13 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  setupFiles: ['./src/setupTests.ts'],
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
   // extended assertions to Jest
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect',
     './src/test-configuration/Expectations.ts',
+    './src/setupTests.ts',
   ],
 
   // Test spec file resolution pattern
