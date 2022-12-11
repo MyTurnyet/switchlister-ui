5682a290a1e62b256ad10d3f78d7781355ab32d7
Paige Watson <paige.watson@outlook.com>
2022-12-10 18:24:35 -0800

    rename

--- src/models/collections/TrainCollection.ts
+++ src/models/collections/TrainCollection.ts
@@ -2,8 +2,8 @@ import { ItemCollection } from './ItemCollection';
 import { Train } from '../Train';
 
 export class TrainCollection extends ItemCollection<Train> {
-  findWithId(expectedId: string): Train {
-    const returnedTrain = this.items.find((train) => train.id === expectedId);
+  findWithId(idToFind: string): Train {
+    const returnedTrain = this.items.find((train) => train.id === idToFind);
     if (returnedTrain === undefined) {
       return Train.EMPTY_TRAIN;
     }
