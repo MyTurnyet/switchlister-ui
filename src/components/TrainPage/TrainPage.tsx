bd7c4ad279866cb68f3642bfbfafcc6e8d6407a2
Paige Watson <paige.watson@outlook.com>
2022-12-10 18:51:14 -0800

    map industry names into a div

--- src/components/TrainPage/TrainPage.tsx
+++ src/components/TrainPage/TrainPage.tsx
@@ -3,13 +3,17 @@ import { useParams } from 'react-router';
 import { useTrainsData } from '../../data/TrainsContext';
 import { Train } from '../../models/Train';
 import { useReactState } from '../../state-management/ReactState';
+import { Station } from '../../models/Station';
+import { Industry } from '../../models/Industry';
 
-export const TrainStationDetails = (props: { stationName: string }) => (
+export const TrainStationDetails = (props: { station: Station }) => (
   <div>
-    <div>Station: {props.stationName}</div>
+    <div>Station: {props.station.name}</div>
+    <div>Industries: ({props.station.industries.count})</div>
     <ul>
-      <li>Industry Name 1</li>
-      <li>Industry Name 2</li>
+      {props.station.industries.map((industry: Industry) => (
+        <li key={industry.name}>{industry.name}</li>
+      ))}
     </ul>
   </div>
 );
@@ -17,8 +21,8 @@ export const TrainDetails = (props: { train: Train }) => {
   return (
     <div>
       <div>{props.train.name}</div>
-      {props.train.stations.stationNames.map((stationName: string) => (
-        <TrainStationDetails stationName={stationName} key={stationName} />
+      {props.train.stations.map((station: Station) => (
+        <TrainStationDetails station={station} key={station.name} />
       ))}
     </div>
   );
