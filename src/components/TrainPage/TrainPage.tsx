a1cba1ba3e6c3d1919d4272c2e216faeb7af5f59
Paige Watson <paige.watson@outlook.com>
2022-12-10 23:25:53 -0800

    add styling to the trains page

--- src/components/TrainPage/TrainPage.tsx
+++ src/components/TrainPage/TrainPage.tsx
@@ -8,20 +8,20 @@ import { Industry } from '../../models/Industry';
 import styled from 'styled-components';
 
 export const TrainStationDetails = (props: { station: Station }) => (
-  <div>
-    <div>Station: {props.station.name}</div>
-    <div>Industries: ({props.station.industries.count})</div>
+  <StationDetailsContainer>
+    <StationName>Station: {props.station.name}</StationName>
+    <IndustriesTitle>Industries: ({props.station.industries.count})</IndustriesTitle>
     <ul>
       {props.station.industries.map((industry: Industry) => (
         <li key={industry.name}>{industry.name}</li>
       ))}
     </ul>
-  </div>
+  </StationDetailsContainer>
 );
 export const TrainDetails = (props: { train: Train }) => {
   return (
     <TrainDetailsContainer>
-      <div>{props.train.name}</div>
+      <TrainName>{props.train.name}</TrainName>
       {props.train.stations.map((station: Station) => (
         <TrainStationDetails station={station} key={station.name} />
       ))}
@@ -47,8 +47,6 @@ export const TrainPage = () => {
 };
 
 const TrainPageContainer = styled.div`
-  background-color: honeydew;
-
   padding-top: 15px;
   padding-bottom: 35px;
   min-height: 95%;
@@ -62,4 +60,27 @@ const TrainPageTitle = styled.div`
 const TrainDetailsContainer = styled.div`
   padding-top: 15px;
   padding-bottom: 45px;
+`;
+const TrainName = styled.div`
+  color: ${(props) => props.theme.colors.text.normal};
+  font-weight: bold;
+  font-size: medium;
+`;
+const StationDetailsContainer = styled.div`
+  padding-left: 12px;
+  padding-top: 12px;
+  & ul {
+    padding-left: 10px;
+    list-style: none;
+  }
+`;
+const StationName = styled.div`
+  color: ${(props) => props.theme.colors.text.normal};
+  font-weight: bold;
+  font-size: medium;
+  border-bottom: ${(props) => props.theme.colors.main} 1px solid;
+`;
+const IndustriesTitle = styled.div`
+  color: ${(props) => props.theme.colors.text.normal};
+  font-size: medium;
 `;
