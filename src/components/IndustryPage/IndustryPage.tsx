7852cbce42f435cb20f9dd5a1098142a00ce31b1
Paige Watson <paige.watson@outlook.com>
2022-12-12 23:29:04 -0800

    show serviced car types

--- src/components/IndustryPage/IndustryPage.tsx
+++ src/components/IndustryPage/IndustryPage.tsx
@@ -1,9 +1,16 @@
 import { Industry } from '../../models/Industry';
+import { CarType } from '../../models/RollingStock';
 
 export const IndustryPage = (props: { industry: Industry }) => {
   return (
     <div>
       <div>{props.industry.name}</div>
+      <div>
+        <div>Car types accepted at this industry:</div>
+        {props.industry.servicedCarTypes.map((type: CarType) => (
+          <div key={type}>{type}</div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/IndustryPage/__tests__/IndustryPage.test.tsx
+++ src/components/IndustryPage/__tests__/IndustryPage.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import { IndustryPage } from '../IndustryPage';
-import { industry6 } from '../../../test-configuration/FixtureTrains';
+import { industry7 } from '../../../test-configuration/FixtureTrains';
 
 describe('Industry Page', () => {
+  let industryPage: RenderResult;
+  beforeEach(() => {
+    industryPage = render(<IndustryPage industry={industry7} />);
+  });
   it('displays the name', () => {
-    const industryPage = render(<IndustryPage industry={industry6} />);
-    expect(industryPage).toHaveElementsWithText(industry6.name);
+    expect(industryPage).toHaveElementsWithText(industry7.name);
+  });
+  it('has serviced car types', () => {
+    expect(industryPage).toHaveElementsWithText('Car types accepted at this industry:', 'XM', 'HT');
   });
 });
