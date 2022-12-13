8a4241f3eeecbbe9e00d66369923a7257bdefeca
Paige Watson <paige@industriallogic.com>
2022-12-13 09:49:36 -0800

    style the IndustryDetails.tsx

--- src/components/IndustryPage/IndustryDetails.tsx
+++ src/components/IndustryPage/IndustryDetails.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import { Industry } from '../../models/Industry';
 import useCollapse from 'react-collapsed';
+import styled from 'styled-components';
+
+interface ExpandedProps {
+  readonly isExpanded: boolean;
+}
 
 export const IndustryDetails = (props: { industry: Industry }) => {
-  const { getToggleProps, getCollapseProps } = useCollapse();
+  const { getToggleProps, getCollapseProps, isExpanded } = useCollapse();
   const carTypes = props.industry.servicedCarTypes;
   return (
-    <div>
-      <div {...getToggleProps()}>{props.industry.name}</div>
+    <IndustryDetailsContainer isExpanded={isExpanded}>
+      <IndustryNameExpander {...getToggleProps()} isExpanded={isExpanded}>
+        {props.industry.name}
+      </IndustryNameExpander>
       <div {...getCollapseProps()}>
         <div>Car types accepted at this industry:</div>
         {carTypes.map((carType) => (
           <div key={carType.toString()}>{carType.toString()}</div>
         ))}
       </div>
-    </div>
+    </IndustryDetailsContainer>
   );
 };
+const IndustryNameExpander = styled.div<ExpandedProps>`
+  cursor: pointer;
+  font-weight: bold;
+  font-size: medium;
+  ${(props) => {
+    return (
+      props.isExpanded &&
+      `border-bottom: ${props.theme.colors.cardBorder} 2px solid;         
+            `
+    );
+  }}
+`;
+const IndustryDetailsContainer = styled.div<ExpandedProps>`
+  margin-top: 
+  margin-bottom: ${(props) => props.isExpanded && '1'}2px;
+  width: 50%;
+`;
