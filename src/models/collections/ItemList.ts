f07db8417c6bb0e39593264d39e17f62f40d305a
Paige Watson <paige.watson@outlook.com>
2022-12-12 23:27:32 -0800

    Add map function

--- src/models/collections/ItemList.ts
+++ src/models/collections/ItemList.ts
@@ -3,4 +3,9 @@ export abstract class ItemList<T> {
   contains(expected: T): boolean {
     return this.items.some((value: T) => value === expected);
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  map(callBackFunction: (item: T, index?: number) => any): any {
+    return this.items.map(callBackFunction);
+  }
 }
