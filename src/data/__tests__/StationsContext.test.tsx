ae42990475318c8a3b272ba4a71f97b44327fe3d
Paige Watson <paige@industriallogic.com>
2022-12-14 07:56:12 -0800

    add button to click to consumer component

--- src/data/__tests__/StationsContext.test.tsx
+++ src/data/__tests__/StationsContext.test.tsx
@@ -4,6 +4,7 @@ import { Station, StationState } from '../../models/Station';
 import { render, RenderResult, waitFor } from '@testing-library/react';
 import { mswServer } from '../../api-mocks/msw-server';
 import { ApiHandler } from '../../api-mocks/handlers/ApiHandler';
+import { clickButtonWithText } from '../../test-configuration/ReactTestToolkit';
 
 const StationsTestConsumer = () => {
   const { stationsCollection, getStations } = useStationsData();
@@ -12,12 +13,19 @@ const StationsTestConsumer = () => {
     getStations();
   });
 
+  const handleClick = () => {
+    return;
+  };
+
   return (
     <div>
       <div>count: {stationsCollection.count}</div>
       {stationsCollection.map((station: Station) => (
         <div key={station.name}>{station.name}</div>
       ))}
+      <button title={'add industry'} onClick={handleClick}>
+        add industry
+      </button>
     </div>
   );
 };
@@ -31,17 +39,25 @@ function renderStationConsumer(): RenderResult {
 }
 
 describe('Stations Context', () => {
-  it('has no stations', async () => {
-    mswServer.use(ApiHandler.createApiCall<StationState[]>('stations', []));
-    const stationConsumer = renderStationConsumer();
-    await waitFor(() => {
-      expect(stationConsumer).toHaveElementsWithText('count: 0');
+  describe('outputs', () => {
+    it('has no stations', async () => {
+      mswServer.use(ApiHandler.createApiCall<StationState[]>('stations', []));
+      const stationConsumer = renderStationConsumer();
+      await waitFor(() => {
+        expect(stationConsumer).toHaveElementsWithText('count: 0');
+      });
+    });
+    it('has four stations', async () => {
+      const stationConsumer = renderStationConsumer();
+      await waitFor(() => {
+        expect(stationConsumer).toHaveElementsWithText('count: 4');
+      });
     });
   });
-  it('has four stations', async () => {
-    const stationConsumer = renderStationConsumer();
-    await waitFor(() => {
-      expect(stationConsumer).toHaveElementsWithText('count: 4');
+  describe('functions', () => {
+    it('adds an industry to the station', () => {
+      const stationConsumer = renderStationConsumer();
+      clickButtonWithText(stationConsumer, 'add industry');
     });
   });
 });
