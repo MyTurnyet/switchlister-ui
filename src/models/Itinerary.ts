03678b39d51cea8ff900de1a7c17037ce036b3ea
Paige Watson <paige.watson@outlook.com>
2023-01-14 21:48:26 -0800

    set first station in constructor

--- src/models/Itinerary.ts
+++ src/models/Itinerary.ts
@@ -3,9 +3,12 @@ import { Route } from './Route';
 import { Station } from './Station';
 
 export class Itinerary {
-  constructor(private train: Train, private route: Route) {}
+  private trainLocation: Station;
+  constructor(private train: Train, private route: Route) {
+    this.trainLocation = route.stations[0];
+  }
 
   currentTrainLocation(): Station {
-    return this.route.stations[0];
+    return this.trainLocation;
   }
 }
