3ddd6859ed5aaafb9842f5841b6e1e256a995f6d
Paige Watson <paige.watson@outlook.com>
2023-02-18 15:53:16 -0800

    add isLastStation to Itinerary

--- src/models/Itinerary.ts
+++ src/models/Itinerary.ts
@@ -4,11 +4,20 @@ import { Station } from './Station';
 
 export class Itinerary {
   private trainLocation: Station;
+
   constructor(private train: Train, private route: TrainRoute) {
     this.trainLocation = route.stations[0];
   }
 
+  get name(): string {
+    return this.route.name;
+  }
+
   currentTrainLocation(): Station {
     return this.trainLocation;
+  }
+
+  isAtLastStation(): boolean {
+    return this.route.stations.at(-1)!.id === this.currentTrainLocation().id;
   }
 }
--- src/models/__tests__/Itinerary.test.ts
+++ src/models/__tests__/Itinerary.test.ts
@@ -2,20 +2,42 @@ import { Itinerary } from '../Itinerary';
 import { train1 } from '../../test-configuration/FixtureTrains';
 import { RouteState, TrainRoute } from '../TrainRoute';
 import { Train } from '../Train';
-import { station1, station1State } from '../../test-configuration/FixtureStations';
+import { station1, station1State, station2State } from '../../test-configuration/FixtureStations';
 
 describe('Itinerary', () => {
   let train: Train;
   let itinerary: Itinerary;
-  const routeState: RouteState = { id: '', name: '', stations: [station1State] };
-  const route = new TrainRoute(routeState);
 
-  beforeEach(() => {
-    train = train1;
-    itinerary = new Itinerary(train, route);
+  describe('with only on station', () => {
+    const routeState: RouteState = {
+      id: 'someId',
+      name: 'Local Switcher',
+      stations: [station1State],
+    };
+    const route = new TrainRoute(routeState);
+    beforeEach(() => {
+      train = train1;
+      itinerary = new Itinerary(train, route);
+    });
+    it('returns correct name', () => {
+      expect(itinerary.name).toEqual('Local Switcher');
+    });
+    it('current location is first station', () => {
+      expect(itinerary.currentTrainLocation()).toEqual(station1);
+    });
+    it('is at last station', () => {
+      expect(itinerary.isAtLastStation()).toEqual(true);
+    });
   });
-  it('takes a train and a route', () => {
-    const trainLocation = itinerary.currentTrainLocation();
-    expect(trainLocation).toEqual(station1);
+  describe('with multiple stations', () => {
+    const routeState: RouteState = { id: '', name: '', stations: [station1State, station2State] };
+    const route = new TrainRoute(routeState);
+    beforeEach(() => {
+      train = train1;
+      itinerary = new Itinerary(train, route);
+    });
+    it('is not at last station', () => {
+      expect(itinerary.isAtLastStation()).toEqual(false);
+    });
   });
 });
